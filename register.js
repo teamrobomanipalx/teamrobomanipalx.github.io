efdc29ee3e58eb6dcfd32a57ac6c67f9d7433ed3
Kishor Ingale <34228995+XXXMARTAND@users.noreply.github.com>
2019-12-01 16:19:48 +0530

    Delete register.js

--- register.js
+++
@@ -1,7 +0,0 @@
-var firebase = app_firebase;
-function register(){
-    document.getElementById("statusbox").innerHTML = "You don't have access";
-    document.getElementById("form").style.visibility = "hidden";
-    document.getElementById("approval").style.visibility = "hidden";
-
-}
