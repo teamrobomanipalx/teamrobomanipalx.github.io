b51dea8ec3086f16e0412617ad6b7b0fbef20dac
Kishor Ingale <34228995+XXXMARTAND@users.noreply.github.com>
2019-12-01 16:18:09 +0530

    Delete log.js

--- log.js
+++
@@ -1,8 +0,0 @@
-var firebase = app_firebase;
-function log(){
-    document.getElementById("statusbox").innerHTML = "You don't have access";
-    document.getElementById("form").style.visibility = "hidden";
-    document.getElementById("approval").style.visibility = "hidden";
-    document.getElementById("approvaltable").remove();
-
-}
