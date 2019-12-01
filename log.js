3fdc71e64cfc3bf5138eb51935a42da07af1d3b4
Kishor Ingale <34228995+XXXMARTAND@users.noreply.github.com>
2019-12-01 13:17:14 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,7 @@
+var firebase = app_firebase;
+function log(){
+    document.getElementById("statusbox").innerHTML = "You don't have access";
+    document.getElementById("form").style.visibility = "hidden";
+    document.getElementById("approval").style.visibility = "hidden";
+
+}
---
+++ register.js
@@ -0,0 +1,7 @@
+var firebase = app_firebase;
+function register(){
+    document.getElementById("statusbox").innerHTML = "You don't have access";
+    document.getElementById("form").style.visibility = "hidden";
+    document.getElementById("approval").style.visibility = "hidden";
+
+}
